6fc638cc806823f17f177fd0fc4e74e5d6621206
michael-vibert <mike160384@hotmail.com>
2022-12-18 12:03:31 +0800

    Small changes including deleting extra files

--- src/install.sh
+++ src/install.sh
@@ -5,7 +5,7 @@ if ! [[ -x "$(command -v python)" ]]
 then
     echo 'Error: 
     This program runs on Python, but it looks like Python is not installed.
-    To install Python, check out https://installpython3.com/' >&2
+    To install Python, check out https://python.org/downloads/' >&2
     exit 1
 else
     echo 'You have the correct Python program to run this program - Lets Go!'
